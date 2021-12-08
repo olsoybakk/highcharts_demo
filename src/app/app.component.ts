5f0c0192528fbc6ae61a3d2a452fddd0632851c0
Terje Olsøybakk <terje.olsoybakk@artsdatabanken.no>
2021-12-08 18:30:41 +0100

    error-handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,74 +42,80 @@ export class AppComponent {
   ngOnInit(): void {
     fetch('https://thingproxy.freeboard.io/fetch/https://www.nordpoolgroup.com/api/marketdata/chart/23?currency=NOK')
     .then(response => response.text())
-    .then(d => {
-      // console.log('d', typeof d, d.length);
-      let data = JSON.parse(d);
-      // console.log('data', data);
-      // console.log(data.data.Rows.forEach((row: any, i: number) => console.log(i,row.Columns)));
-      // console.log(this.chartOptions);
-      const values: any = {};
-      let startTime: Date;
-      data.data.Rows.forEach((row: any, i: number) => {
-        if (startTime === undefined) startTime = row.StartTime;
-        let dateRow = (new Date(row.StartTime)).getTime();
-        // let dateRow = row.StartTime;
-        row.Columns.forEach((column: any, j: number) => {
-          if (values[column.Name] === undefined) values[column.Name] = [];
-          let value = parseFloat(column.Value.replace(' ', '').replace(',', '.'));
-          // values[column.Name].unshift([dateRow, value]);
-          values[column.Name].push([dateRow, value]);
-        });
-      });
-      // console.log('values', values);
-      const series: any[] = [];
-      for (var key in values) {
-        // console.log(key);
-        // console.log(key, values[key]);
-        series.push({
-          name: key,
-          type: 'line',
-          data: values[key].sort((a:any[],b:any[]) => a[0] - b[0])
-        });
-      }
-      // console.log('series', series);
+    .then(d => this.fillChart(d))
+    .catch(() => {
+      fetch('assets/23.json')
+      .then(response => response.text())
+      .then(d => this.fillChart(d))
+    });
+  }
 
-      let options: Highcharts.Options = {
-        chart: {
-          zoomType: 'x'
+  fillChart(d: string): void {
+    // console.log('d', typeof d, d.length);
+    let data = JSON.parse(d);
+    // console.log('data', data);
+    // console.log(data.data.Rows.forEach((row: any, i: number) => console.log(i,row.Columns)));
+    // console.log(this.chartOptions);
+    const values: any = {};
+    let startTime: Date;
+    data.data.Rows.forEach((row: any, i: number) => {
+      if (startTime === undefined) startTime = row.StartTime;
+      let dateRow = (new Date(row.StartTime)).getTime();
+      // let dateRow = row.StartTime;
+      row.Columns.forEach((column: any, j: number) => {
+        if (values[column.Name] === undefined) values[column.Name] = [];
+        let value = parseFloat(column.Value.replace(' ', '').replace(',', '.'));
+        // values[column.Name].unshift([dateRow, value]);
+        values[column.Name].push([dateRow, value]);
+      });
+    });
+    // console.log('values', values);
+    const series: any[] = [];
+    for (var key in values) {
+      // console.log(key);
+      // console.log(key, values[key]);
+      series.push({
+        name: key,
+        type: 'line',
+        data: values[key].sort((a:any[],b:any[]) => a[0] - b[0])
+      });
+    }
+    // console.log('series', series);
+
+    let options: Highcharts.Options = {
+      chart: {
+        zoomType: 'x'
+      },
+      title: {
+        text: `Nordpool data - ${data.header.title} ${(new Date(data.data.LatestResultDate)).toLocaleString()}`
+      },
+      xAxis: {
+        type: 'datetime',
+        dateTimeLabelFormats: { // don't display the dummy year
+          month: '%m',
+          year: '%Y'
         },
         title: {
-          text: `Nordpool data - ${data.header.title} ${(new Date(data.data.LatestResultDate)).toLocaleString()}`
-        },
-        xAxis: {
-          type: 'datetime',
-          dateTimeLabelFormats: { // don't display the dummy year
-            month: '%m',
-            year: '%Y'
-          },
+            text: 'Date'
+        }
+      },
+      yAxis: {
           title: {
-              text: 'Date'
+              text: `Electric rate (${data.data.Units})`
+          },
+          min: 0
+      },
+      plotOptions: {
+        series: {
+          marker: {
+              enabled: false
           }
-        },
-        yAxis: {
-            title: {
-                text: `Electric rate (${data.data.Units})`
-            },
-            min: 0
-        },
-        plotOptions: {
-          series: {
-            marker: {
-                enabled: false
-            }
-          }
-        },
-        // colors: ['#6CF', '#39F', '#06C', '#036', '#000'],
-        // series: [series[3]]
-        series: series
-      };
-      this.chartOptions = options;
-
-    });
+        }
+      },
+      // colors: ['#6CF', '#39F', '#06C', '#036', '#000'],
+      // series: [series[3]]
+      series: series
+    };
+    this.chartOptions = options;
   }
 }
