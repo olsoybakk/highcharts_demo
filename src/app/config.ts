a27e7373d305af13ed286d3ea9bfde7c431c47af
Terje Olsøybakk <terje.olsoybakk@artsdatabanken.no>
2021-12-15 15:48:37 +0100

    .

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 const config = {
-    'buildDate': '2021.12.14',
-    'buildTime': '21:29',
+    'buildDate': '2021.12.15',
+    'buildTime': '15:48',
     // 'elspotUrl': 'https://nve.geodataonline.no/arcgis/rest/services/Mapservices/Elspot/MapServer',
     // 'elspotLayer': '0'
 };
