eb1d4b87b6bdc672c94084623848f0d4dd59beda
olsoybakk <terje@olsoybakk.no>
2021-12-17 23:59:11 +0100

    .

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 const config = {
     'buildDate': '2021.12.17',
-    'buildTime': '22:55',
+    'buildTime': '23:58',
     // 'elspotUrl': 'https://nve.geodataonline.no/arcgis/rest/services/Mapservices/Elspot/MapServer',
     // 'elspotLayer': '0'
 };
