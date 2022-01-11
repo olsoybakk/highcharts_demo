f469cb426b047713b125a450edf238d47aac6e03
Terje Olsøybakk <terje.olsoybakk@artsdatabanken.no>
2022-01-11 14:19:09 +0100

    full ncu update

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 const config = {
-    'buildDate': '2021.12.27',
-    'buildTime': '09:38',
+    'buildDate': '2022.01.11',
+    'buildTime': '14:00',
     // 'elspotUrl': 'https://nve.geodataonline.no/arcgis/rest/services/Mapservices/Elspot/MapServer',
     // 'elspotLayer': '0'
 };
