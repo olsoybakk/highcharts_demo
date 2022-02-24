ebf361c751b2b133ae78261511e2ee60f0bde703
terje.olsoybakk@miles.no <terje.olsoybakk@miles.no>
2022-02-24 21:49:03 +0100

    updates

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,8 +1,12 @@
 const config = {
-    'buildDate': '2022.02.21',
-    'buildTime': '23:00',
+    'buildDate': '2022.02.24',
+    'buildTime': '20:00',
     // 'elspotUrl': 'https://nve.geodataonline.no/arcgis/rest/services/Mapservices/Elspot/MapServer',
     // 'elspotLayer': '0'
 };
+
+// https://nve.geodataonline.no/arcgis/rest/services/Mapservices/Elspot/MapServer/0/query
+// https://nve.geodataonline.no/arcgis/rest/services/Mapservices/Elspot/MapServer/0/query?where=OBJECTID+%3E+0&text=&objectIds=&time=&timeRelation=esriTimeRelationOverlaps&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=&returnGeometry=true&returnTrueCurves=true&maxAllowableOffset=&geometryPrecision=&outSR=25832&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=true&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&sqlFormat=none&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=geojson
+// https://nve.geodataonline.no/arcgis/rest/services/Mapservices/Elspot/MapServer/0/query?where=OBJECTID+%3E+0&text=&objectIds=&time=&timeRelation=esriTimeRelationOverlaps&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Meter&relationParam=&outFields=&returnGeometry=true&returnTrueCurves=true&maxAllowableOffset=&geometryPrecision=&outSR=25832&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=true&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&sqlFormat=none&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=geojson
 
 export default config;
