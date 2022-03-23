ee1b6c9da43ef349ca3e7b3bb4f0ed48d0a13098
Terje Olsøybakk <terje.olsoybakk@miles.no>
2022-03-23 22:32:04 +0100

    .

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 const config = {
-    'buildDate': '2022.03.18',
-    'buildTime': '10:50',
+    'buildDate': '2022.03.23',
+    'buildTime': '22:30',
     // 'elspotUrl': 'https://nve.geodataonline.no/arcgis/rest/services/Mapservices/Elspot/MapServer',
     // 'elspotLayer': '0'
 };
