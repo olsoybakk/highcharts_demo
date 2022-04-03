938ae3dc9539a25f16469ac872cda3a1b2ed9c07
Terje Olsøybakk <terje.olsoybakk@miles.no>
2022-04-03 15:35:12 +0200

    moment v2.29.2

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 const config = {
-    'buildDate': '2022.04.01',
-    'buildTime': '22:11',
+    'buildDate': '2022.04.03',
+    'buildTime': '15:34',
     // 'elspotUrl': 'https://nve.geodataonline.no/arcgis/rest/services/Mapservices/Elspot/MapServer',
     // 'elspotLayer': '0'
 };
